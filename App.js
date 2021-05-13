2f3650695ba1910b7eb34c2b9b70286bd34dc907
Tim Garrity <timgarrity@timg-a01.vmware.com>
2021-05-13 09:00:32 -0400

    Home screen made

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import 'react-native-gesture-handler'
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Dimensions } from 'react-native';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
 import PercentageScreen from './Screens/PercentageScreen';
+import HomeScreen from './Screens/HomeScreen';
 
 const Stack = createStackNavigator()
 
@@ -12,8 +12,25 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Welcome' }}
+        />
         <Stack.Screen 
-          name="Percentage Screen"
+          name="DeadLift Screen"
+          component={PercentageScreen}
+        />
+        <Stack.Screen 
+          name="Squat Screen"
+          component={PercentageScreen}
+        />
+        <Stack.Screen 
+          name="Bench Press Screen"
+          component={PercentageScreen}
+        />
+        <Stack.Screen 
+          name="Shoulder Press Screen"
           component={PercentageScreen}
         />
       </Stack.Navigator>
